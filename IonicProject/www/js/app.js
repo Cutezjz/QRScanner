560b967bd7c30207fc996a4041b28ec9fd1258ef
JZ Zhang <cutezjz@gmail.com>
2016-05-04 19:13:17 -0500

    Uploaded
    1st Upload

---
+++ IonicProject/www/js/app.js
@@ -0,0 +1,54 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+angular.module('starter', ['ionic','ngCordova'])
+
+.run(function($ionicPlatform) {
+	
+	
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs).
+    // The reason we default this to hidden is that native apps don't usually show an accessory bar, at
+    // least on iOS. It's a dead giveaway that an app is using a Web View. However, it's sometimes
+    // useful especially with forms, though we would prefer giving the user a little more room
+    // to interact with the app.
+    if (window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+    }
+    if (window.StatusBar) {
+      // Set the statusbar to use the default style, tweak this to
+      // remove the status bar on iOS or change it to use white instead of dark colors.
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.controller("ScanController", function($scope, $cordovaBarcodeScanner) {
+	$scope.format = "";
+	$scope.cancelled = "";
+	$scope.text = "555";
+ 
+    $scope.scanBarcode = function() {
+        $cordovaBarcodeScanner.scan().then(function(imageData) {
+			$scope.text = imageData.text;
+			$scope.format = imageData.format;
+			$scope.cancelled = imageData.cancelled;
+			/*
+            alert(imageData.text);
+            console.log("Barcode Format -> " + imageData.format);
+            console.log("Cancelled -> " + imageData.cancelled);
+			*/
+        }, function(error) {
+            console.log("An error happened -> " + error);
+        });
+    };
+ 
+})
+
+;
+
+
